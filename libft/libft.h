4862cfb0303f3b761eedd423f8365ee34ec81c03
kane <pro.kane.io@gmail.com>
2023-11-08 15:29:53 +0100

    end of ft_strlen

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkane <mkane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:12:28 by mkane             #+#    #+#             */
-/*   Updated: 2023/11/08 15:00:53 by mkane            ###   ########.fr       */
+/*   Updated: 2023/11/08 15:17:37 by mkane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ int	ft_isdigit(int c);
 int ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_strlen(char *str);
 #endif
--- libft/main.c
+++ libft/main.c
@@ -6,17 +6,18 @@
 /*   By: mkane <mkane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:26:44 by mkane             #+#    #+#             */
-/*   Updated: 2023/11/08 15:09:30 by mkane            ###   ########.fr       */
+/*   Updated: 2023/11/08 15:29:23 by mkane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "libft.h"
 
 int main()
 {
-	int i =0;
+	// int i =0;
 	
 	// testisalpha
 	// while (i < 256)
@@ -77,14 +78,17 @@ int main()
 	// 	i++;
 	// }
 
-	while (i < 256)
-	{
-
-		int my_function = ft_isprint(i);
-		int true_function = isprint(i);
-		printf("%d\n", my_function);
-		printf("%d\n", true_function);
-		printf("==============================\n");
-		i++;
-	}
+	// teststrlen
+	// printf("%d\n", ft_strlen("salut"));
+	// printf("%lu\n", strlen("salut"));
+	// printf("==============================\n");
+	// printf("%d\n", ft_strlen("ca va "));
+	// printf("%lu\n", strlen("ca va "));
+	// printf("==============================\n");
+	// printf("%d\n", ft_strlen("42 est top"));
+	// printf("%lu\n", strlen("42 est top"));
+	// printf("==============================\n");
+	// printf("%d\n", ft_strlen("hello world"));
+	// printf("%lu\n", strlen("hello world"));
+	// printf("==============================\n");
 }
